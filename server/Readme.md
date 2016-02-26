fdff7538d31ffbf7d71ce1c79d2970c27a458ae1
Quark Li <quarkli@gmail.com>
2016-02-27 01:29:04 +0800

    Remove msg_srouter from PeerCom which is now in its own repository

--- server/Readme.md
+++
@@ -1,8 +0,0 @@
-<h2>Message Router</h2>
-
-<h3>Introduction</h3>
-
-<h3>Installation</h3>
-
-<h3>Dependency</h3>
-
--- server/config.rb
+++
@@ -1,4 +0,0 @@
-#LOGFILE = "<path and filename to your log file>"
-#PRIVATEKEY = "<path and filename to your https server private key>"
-#CERTIFICATE = "<path and filename to your https server certificate>"
-#GEOFILE = "<path and filename to your geo location file>"
